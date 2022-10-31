651c1a6aefc3ee098459c810e4598a90c1e84cca
jsungyoo <dyeonkim-md@dyeonkim-MDui-Mac-Studio.local>
2022-10-31 11:56:13 +0900

    ts,vue만 적용되도록

--- src/components/HelloWorld.ts
+++ src/components/HelloWorld.ts
@@ -3,6 +3,11 @@ import {Component, Prop, Vue} from 'vue-property-decorator';
 @Component
 export default class HelloWorld extends Vue {
     @Prop() protected msg!: string;
+
+    protected num = () => {
+        console.log('protected');
+    };
+
     private test = () => {
         console.log('test');
     };
